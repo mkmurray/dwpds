2065baf335688e91d26e1977cb0dc8bfc93d294c
Michael Bradley <eMicBra@gmail.com>
2011-05-19 18:28:47 -0600

    Pushing new changes

--- source/nothinbutdotnetstore.specs/ItemDetailSpecifierSpecs.cs
+++ source/nothinbutdotnetstore.specs/ItemDetailSpecifierSpecs.cs
@@ -1,6 +1,8 @@
+ using System.Linq.Expressions;
  using Machine.Specifications;
  using developwithpassion.specifications.rhinomocks;
  using developwithpassion.specifications.extensions;
+ using nothinbutdotnetstore.infrastructure;
  using nothinbutdotnetstore.web.core;
  using nothinbutdotnetstore.web.core.urls;
 
@@ -17,15 +19,29 @@ namespace nothinbutdotnetstore.specs
         [Subject(typeof(ISpecifyItemDetails<OurModel>))]
         public class wheb_including_an_item : concern
         {
+            private Establish c = () =>
+                                      {
+                                          theModel = new OurModel();
+                                          propertyAccessor = x => x.name;
+                                      };
+
+            private Because b = () =>
+                                    {
+                                        sut.item(propertyAccessor);
+                                    };
+
 
             private It needs_to_create_a_name_value_pair_for_the_token_store = () => { };
-            private It should_invoke_the_property_accessor_against_the_input_model = () => { };
 
+            private It should_invoke_the_property_accessor_against_the_input_model = () => { };
+            private static OurModel theModel;
+            private static Expression<PropertyAccessor<OurModel, string>> propertyAccessor;
+            private static bool invokedPropertyAccessor;
         }
 
         public class OurModel
         {
-            
+            public string name;
         }
     }
 }
