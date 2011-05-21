a54a86db5b121da9a3ba3c905b3e4d5f9a77d211
Brad Mead <bradmead@gmail.com>
2011-05-20 21:11:14 -0700

    Pushing new changes

--- source/nothinbutdotnetstore/infrastructure/container/DependencyFactoryNotRegisteredException.cs
+++ source/nothinbutdotnetstore/infrastructure/container/DependencyFactoryNotRegisteredException.cs
@@ -7,6 +7,7 @@ namespace nothinbutdotnetstore.infrastructure.container
     public Type type_that_has_no_factory { get; private set; }
 
     public DependencyFactoryNotRegisteredException(Type type_without_factory)
+      : base(string.Format("There is no factory registered for type: {0}", type_without_factory.FullName))
     {
       this.type_that_has_no_factory = type_without_factory;
     }
